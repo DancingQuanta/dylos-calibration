bd2263f574732d84176882ab50d52df0cbbb1e01
DancingQuanta <andytheseeker@gmail.com>
2016-09-04 15:53:39 +0100

    Modified grimm-test.p to reflect changes to modules/*.py

--- src/sensors/grimm-test.py
+++ src/sensors/grimm-test.py
@@ -1,32 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from grimm import *
+import os
+from src.modules.data import *
+from src.modules.calibration import *
 from datetime import datetime as dt
 
 start = dt(2016, 7, 4, 10, 30)
 end = dt(2016, 7, 4, 10, 35)
 
 print("Grimm")
-project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                           os.pardir,
+                                           os.pardir))
+
 path = os.path.join(project_dir, "data", "raw", "grimm.log")
-grimmData = loadGrimmData(path).loc[start:end]
-grimmData = grimmData.resample('T',label='left', closed='left').mean()
+
+# Fetch information about bin boundaries for this sensor
+bins = ['', 0.3, 0.4, 0.5, 0.65, 0.8, 1, 1.6, 2, 3, 4, 5, 7.5, 10, 15, 20, 30]
+
+# Load data
+bins = loadData(path, bins, "grimm")
+
+# Select data
+data = bins['data'].loc[start:end]
+
+# Resample data
+data = data.resample('T',label='left', closed='left').mean()
+
 # Grimm data is shifted 7 minutes ahead.
-grimmData.index = grimmData.index - pd.DateOffset(minutes=7)
+data.index = data.index - pd.DateOffset(minutes=7)
 
-mean = grimmData.mean()
+# New bin boundaries
+bins2 = [0.5,2.5,10]
+
+# Rebin grimm data to different bin boundaries
+rebinned = rebin(bins, bins2)
+
+mean = data.mean()
+print(mean)
 meandf = pd.DataFrame([mean])
-bins1 = {'columns': ['0.23-Grimm','0.3-Grimm','0.4-Grimm','0.5-Grimm','0.65-Grimm',
-          '0.8-Grimm','1-Grimm','1.6-Grimm','2-Grimm','3-Grimm','4-Grimm',
-          '5-Grimm','7.5-Grimm','10-Grimm','15-Grimm','20-Grimm'],
-         'bounds': [0.23,0.3,0.4,0.5,0.65,0.8,1,1.6,2,3,4,5,7.5,10,15,20,30]}
+print(meandf)
 
-bins2 = {'columns': ['0.5-Grimm', '2.5-Grimm'],
-         'bounds': [0.5,2.5,30]}
+mean = rebinned.mean()
+print(mean)
+meandf = pd.DataFrame([mean])
+print(meandf)
 
-data = rebin(grimmData,bins1,bins2)
-print(data)
-data = remapGrimm(grimmData)
-print('remap')
-print(data)
