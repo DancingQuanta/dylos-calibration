c383b8761393b16ebfc8e32fd040a908563bac72
DancingQuanta <andytheseeker@gmail.com>
2016-09-09 11:29:18 +0100

    lots of changes, introduced logging modules Now trying to fix calibration

--- src/calibration.py
+++ src/calibration.py
@@ -11,24 +11,66 @@ from src.modules.analysis import *
 from src.modules.calibration import *
 from datetime import datetime, timedelta
 from pint import UnitRegistry
-matplotlib.style.use('ggplot')
+import copy
+import logging
+
+logging.basicConfig(filename='log',
+                    filemode='w',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+
+
+# matplotlib.style.use('ggplot')
 pd.options.display.float_format = '{:.2f}'.format
 
 # matplotlib settings
-params = {'backend': 'pdf',
-          'axes.labelsize': 10,
-          'axes.titlesize': 8,
-          'font.size': 8,
-          'legend.fontsize': 8,
-          'xtick.labelsize': 8,
-          'ytick.labelsize': 8,
-          # 'text.usetex': True,
-          'figure.figsize': [4.2, 3]
-         }
+params = {                      # setup matplotlib to use latex for output
+    "pgf.texsystem": "pdflatex",        # change this if using xetex or lautex
+    "text.latex.unicode": False,
+    # "text.usetex": True,                # use LaTeX to write all text
+    "font.family": "serif",
+    "font.serif": [],                   # blank entries should cause plots to inherit fonts from the document
+    "font.sans-serif": [],
+    "font.monospace": [],
+    "axes.labelsize": 10,               # LaTeX default is 10pt font.
+    'font.size': 10,
+    "legend.fontsize": 8,               # Make the legend/label fonts a little smaller
+    "xtick.labelsize": 8,
+    "ytick.labelsize": 8,
+    "figure.figsize": figsize(0.45),     # default fig size of 0.9 textwidth
+    "pgf.preamble": [
+        r"\usepackage[utf8x]{inputenc}",    # use utf8 fonts becasue your computer can handle it :)
+        r"\usepackage[T1]{fontenc}",        # plots will be generated using this preamble
+        ]
+    }
 matplotlib.rcParams.update(params)
 
+# Create some directories if they do not exist
 
-def loadSensorsData(project_dir, sensors, sensorsFile, outputConc):
+# Base project directory
+project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
+project_dir = os.path.abspath(project_dir)
+
+# Base imgs directory
+base_imgs_dir = os.path.join(project_dir, "imgs", "calibration")
+if not os.path.isdir(base_imgs_dir):
+    os.makedirs(base_imgs_dir)
+
+# Base interim data directory
+base_interim_data_dir = os.path.join(project_dir, "data", "interim",
+                                     "calibration")
+if not os.path.isdir(base_interim_data_dir):
+    os.makedirs(base_interim_data_dir)
+
+# Base processed data directory
+base_processed_data_dir = os.path.join(project_dir, "data", "processed",
+                                     "calibration")
+if not os.path.isdir(base_processed_data_dir):
+    os.makedirs(base_processed_data_dir)
+
+
+def loadSensorsData(sensors, sensorsFile, outputConc):
     # Load sensors definitions
     with open(sensorsFile) as handle:
         sensorDefinition = yaml.load(handle)
@@ -36,8 +78,12 @@ def loadSensorsData(project_dir, sensors, sensorsFile, outputConc):
     # Initialise pint's UnitRegistry
     ureg = UnitRegistry()
 
+    debug = ("Loading sensor data")
+    logging.debug(debug)
+
     # Load data for a sensor
     for sensor in sensors:
+        logging.debug("Sensor: %s" % (sensor))
         # Name the dataset
         sensorName = sensors[sensor]['name']
         # Get settings for this sensor
@@ -75,24 +121,37 @@ def loadSensorsData(project_dir, sensors, sensorsFile, outputConc):
             period = timeshift[1:]
             t = datetime.strptime(period, "%H:%M:%S")
             delta = timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
+            debug = "Time shifted by %s %s" % (sign, str(delta))
+            logging.debug(debug)
+            debug = ("Before timeshift\n"
+                     "Start: %s, end: %s" % (data.index[0], data.index[-1]))
+            logging.debug(debug)
             if sign == '-':
                 data.index = data.index - delta
             elif sign == '+':
                 data.index = data.index + delta
+            else:
+                raise ValueError("Sign not given for timeshift")
+            debug = ("After timeshift\n"
+                     "Start: %s, end: %s" % (data.index[0], data.index[-1]))
+            logging.debug(debug)
 
         # Scaling and unit conversion of particle concentration
         inputConc = settings['concentration']
         scale = float((ureg(outputConc))/(ureg(inputConc)))
-        print(scale)
 
         # Multiply the data with scale factor and update binDate dict
         bins['data'] = data*scale
+        debug = "The scaling factor is %s" % (scale)
+        logging.debug(debug)
 
         # Update settings dict with bins
         settings['bins'] = bins
 
         # Update sensor dict with settings
         sensors[sensor]['sensor'] = settings
+
+    logging.debug("Rebinning calibrater")
 
     # Rebin calibrater data to calibratee bin boundaries
     calibratee = sensors['calibratee']['sensor']['bins']
@@ -105,24 +164,19 @@ def loadSensorsData(project_dir, sensors, sensorsFile, outputConc):
     # rebin calibrater dataset to match calibratee dataset
     rebinned = rebin(calibrater, finalBins)
     name = "rebinned-" + sensors['calibrater']['name']
+    writeData(rebinned['data'], base_interim_data_dir, name)
     sensors['rebinned'] = {}
     sensors['rebinned']['name'] = name
     sensors['rebinned']['sensor'] = {}
     sensors['rebinned']['sensor']['bins'] = rebinned
+    debug = ("Start: %s, end: %s" % (rebinned['data'].index[0],
+                                     rebinned['data'].index[-1]))
+    logging.debug(debug)
     return sensors
 
 
-def experiments(project_dir, expDict, sensors):
+def experiments(expDict, sensors):
     # Experiments
-
-    # Create some directories if they do not exist
-    base_imgs_dir = os.path.join(project_dir, "imgs", "calibration")
-    if not os.path.isdir(base_imgs_dir):
-        os.makedirs(base_imgs_dir)
-    base_interim_data_dir = os.path.join(project_dir, "data", "interim",
-                                         "calibration")
-    if not os.path.isdir(base_interim_data_dir):
-        os.makedirs(base_interim_data_dir)
 
     # # Load experiment information such as times of stimulus
     # with open(expFile) as handle:
@@ -130,21 +184,16 @@ def experiments(project_dir, expDict, sensors):
     order = expDict['order']
     conditions = expDict['conditions']
 
-
-    # Concat rebinned data with calibratee data
-    calibraterData = sensors['rebinned']['sensor']['bins']['data']
-    calibrateeData = sensors['calibratee']['sensor']['bins']['data']
-    calibrationData = concat(calibraterData, calibrateeData)
-
-    # Initize a DataFrame to store mean particle counts of each experiment for
+    # Initialise a DataFrame to store mean particle counts of each experiment for
     # a calibratee and rebinned calibrater
-    index = order
-    columns = calibrationData.columns
-    calibration = pd.DataFrame(index=index, columns=columns)
+    # index = order
+    # columns = calibrationData.columns
+    calibration = pd.DataFrame()
+    caliName = sensors['calibrater']['name'] + "-" + sensors['calibratee']['name']
 
+    logging.debug("Experiment time")
     # Iterate over different experiment conditions
     for exp in order:
-        print(exp)
         condition = conditions[exp]
 
         imgs_dir = os.path.join(base_imgs_dir, exp)
@@ -153,6 +202,9 @@ def experiments(project_dir, expDict, sensors):
         interim_data_dir = os.path.join(base_interim_data_dir, exp)
         if not os.path.isdir(interim_data_dir):
             os.makedirs(interim_data_dir)
+        processed_data_dir = os.path.join(base_processed_data_dir, exp)
+        if not os.path.isdir(processed_data_dir):
+            os.makedirs(processed_data_dir)
 
         # Load start and end datetime for time series data
         time = condition['time']
@@ -162,62 +214,82 @@ def experiments(project_dir, expDict, sensors):
         condition['data'] = {}
 
         # Iterate over different sensors
-        for sensor in sensors:
-            print(sensor)
-            dataDict = {}
-            sensorDict = sensors[sensor]
+        try:
+            for sensor in sensors:
+                    dataDict = {}
+                    sensorDict = sensors[sensor]
 
-            # Name the processed data
-            sensorName = sensorDict['name']
+                    # Name the processed data
+                    sensorName = sensorDict['name']
 
-            # Load data from sensor dict from time period
-            sample = sensorDict['sensor']['bins']
-            sample['data'] = sample['data'].loc[start:end]
+                    # Load data from sensor dict from time period
+                    # Copy the bins data into new dict
+                    sample = copy.deepcopy(sensorDict['sensor']['bins'])
+                    fullstart = sensorDict['sensor']['bins']['data'].index[0]
+                    fullend = sensorDict['sensor']['bins']['data'].index[-1]
+                    debug = ("Exp: %s, Sensor: %s,\n"
+                             "Data start: %s and end: %s\n"
+                             "Sample start: %s and end: %s\n" % (exp, sensor,
+                                                               fullstart,
+                                                               fullend, start, end))
+                    logging.debug(debug)
+                    sample['data'] = sample['data'].loc[start:end]
 
-            # Does data from this sensor need processing?
-            if 'process' in sensorDict:
-                # Check if any bins have zero counts and drop them
-                if 'prunebins' in sensorDict['process']:
-                    # Prune bins from data that have zero counts
-                    sample = pruneBins(sample)
+                    realstart = sample['data'].index[0]
+                    realend = sample['data'].index[-1]
+                    debug = ("Sample start: %s and end: %s\n" % (realstart, realend))
+                    logging.debug(debug)
 
-            # Write sample to file
-            writeData(sample['data'], interim_data_dir, sensorName)
+                    # Does data from this sensor need processing?
+                    if 'process' in sensorDict:
+                        # Check if any bins have zero counts and drop them
+                        if 'prunebins' in sensorDict['process']:
+                            # Prune bins from data that have zero counts
+                            sample = pruneBins(sample)
 
-            # Plot selected time series data
-            plotPath = plot(sample['data'], imgs_dir, sensorName)
-            dataDict["plot"] = plotPath.replace("\\\\", "/")
+                    # Write sample to file
+                    writeData(sample['data'], interim_data_dir, sensorName)
 
-            # Plot histogram and get statistics
-            histDict = histogram(sample, imgs_dir, sensorName)
-            dataDict["histogram"] = histDict
+                    # Plot selected time series data
+                    plotPath = plot(sample['data'], imgs_dir, sensorName)
+                    dataDict["plot"] = plotPath.replace("\\\\", "/")
 
-            # Add data collected so far for this experiment and sensor to main
-            # dictionary
-            condition['data'][sensor] = dataDict
+                    # Plot histogram and get statistics
+                    histDict = histogram(sample, imgs_dir, processed_data_dir, sensorName)
+                    dataDict["histogram"] = histDict
 
-        df = calibrationData.loc[start:end]
-        name = sensors['calibrater']['name'] + sensors['calibratee']['name']
-        plotPath = plot(df, imgs_dir, name)
-        dict = {}
-        dict['plot'] = plotPath.replace("\\\\", "/")
+                    # Add data collected so far for this experiment and sensor to main
+                    # dictionary
+                    condition['data'][sensor] = dataDict
 
-        # Calculate calibration factors
-        calibrated = calibrate(df)
-        calibration.loc[exp] = calibrated
-        dict['data'] = calibration
+            conditions[exp] = condition
+            logging.debug("Calibrating")
+            df = calibrationData.loc[start:end]
+            dict = {}
+            dict['plot'] = plot(df, imgs_dir, caliName)
 
-        condition['data']['calibration'] = dict
-        conditions[exp] = condition
+            # Calculate calibration factors
+            calibrated = calibrate(df)
+            print(calibrated)
+            calibration.loc[exp] = calibrated
+            condition['data']['calibration'] = dict
+
+        except Exception as e:
+            del conditions[exp]
+            order.remove(exp)
+            logging.exception(e)
+            logging.debug(exp)
+            continue
 
     # Add data collected for all experiments and write to file
-    print(calibration)
-    expDict['calibration'] = calibration.to_latex()
+    latex = save_latex(calibration, base_processed_data_dir, caliName, "calibration")
+    expDict['calibration'] = latex
     expDict['conditions'] = conditions
     return expDict
 
 
 if __name__ == '__main__':
+
     # Get filenames to work with
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("settings", help="Settings yaml file")
@@ -228,15 +300,6 @@ if __name__ == '__main__':
     settingsFile = options.settings
     sensorsFile = options.sensors
     outputFile = options.output
-
-    # Base project directory
-    project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
-    project_dir = os.path.abspath(project_dir)
-
-    # Where should processed data go?
-    processed_data_dir = os.path.join(project_dir, "data", "processed")
-    if not os.path.isdir(processed_data_dir):
-        os.makedirs(processed_data_dir)
 
     # Load settings yaml file
     with open(settingsFile) as handle:
@@ -252,11 +315,10 @@ if __name__ == '__main__':
     expDict = settings['exp']
 
     # Load information about sensors and load data for each sensor
-    sensorsDict = loadSensorsData(project_dir, sensorsDict,
-                                  sensorsFile, outputConc)
+    sensorsDict = loadSensorsData(sensorsDict, sensorsFile, outputConc)
 
     # Perform analysis on each experiment
-    expDict = experiments(project_dir, expDict, sensorsDict)
+    expDict = experiments(expDict, sensorsDict)
 
     # Output processed data
     settings['sensors'] = sensorsDict
@@ -265,6 +327,7 @@ if __name__ == '__main__':
     path = os.path.dirname(outputFile)
     if not os.path.isdir(path):
         os.makedirs(path)
+    dump = json.dumps(outputDict, default=date_handler,
+                     sort_keys=True, indent=4).replace("\\\\", "/")
     with open(outputFile, 'w') as outfile:
-        json.dump(outputDict, outfile, default=date_handler,
-                  sort_keys=True, indent=4)
+            outfile.write(dump)
