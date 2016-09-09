20cf2f6ee5f42c40996a343ddb0fda1528527b63
DancingQuanta <andytheseeker@gmail.com>
2016-09-09 18:23:45 +0100

    Added some helper functions and moved and modificed calibrate function.

--- src/modules/analysis.py
+++ src/modules/analysis.py
@@ -10,11 +10,61 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy import stats
+
+def figsize(scale):
+    fig_width_pt = 483.69687                          # Get this from LaTeX using \the\textwidth
+    inches_per_pt = 1.0 / 72.27                       # Convert pt to inch
+    golden_mean = (np.sqrt(5.0)-1.0) / 2.0            # Aesthetic ratio (you could change this)
+    fig_width = fig_width_pt * inches_per_pt*scale    # width in inches
+    fig_height = fig_width * 0.80              # height in inches
+    fig_size = [fig_width, fig_height]
+    return fig_size
+
+
+def saveplot(path, name, kind, fig, **kwargs):
+    filename = name + "-" + kind + ".png"
+    path1 = os.path.join(path, filename)
+    print("Plotting: %s" % (path1))
+    fig.savefig(path1)
+    filename = name + "-" + kind + ".pgf"
+    path2 = os.path.join(path, filename)
+    fig.savefig(path2, **kwargs)
+    return path2.replace("\\\\", "/")
+
+
+def save_latex(df, path, name, kind, **kwargs):
+    """Save a DataFrame to latex table via to_latex()
+
+    Args:
+        df: Pandas.DataFrame
+        path: str
+            where outputfile will be saved to
+        name: str
+            part of filename
+        kind: str
+            part of filename, could be a histogram or statistics
+        kwargs: **
+            pass keywords unto pd.to_latex()
+    Returns:
+        path: str
+            location and filename of saved latex file which can used to input
+            into a latex document with \input(path)
+
+    """
+    latex = df.to_latex(**kwargs)
+    filename = name + "-" + kind +  ".tex"
+    path = os.path.join(path, filename)
+    with open(path, 'w') as f:
+        f.write(latex)
+    return path.replace("\\\\", "/")
+
 
 def nmoment(x, counts, c, n):
     return np.sum(((x-c)**n)*counts) / np.sum(counts)
 
-def stats(midpoints, counts):
+
+def statistics(midpoints, counts):
     """Statistics for binned data
 
     Args:
@@ -29,7 +79,7 @@ def stats(midpoints, counts):
     mean = nmoment(midpoints, counts, 0, 1)
 
     # Standard deviation
-    std = nmoment(midpoints, counts, mean, 2)
+    std = nmoment(midpoints, counts, mean, 2)**0.5
 
     # Lower 95% bounds
     lower = mean - 2 * std
@@ -38,53 +88,6 @@ def stats(midpoints, counts):
     upper = mean + 2 * std
 
     return mean, std, lower, upper
-
-
-def histstd(data, bounds, mean):
-    """Calculate standard deviation of a binned data
-    Args:
-        data : list
-            list of counts per bin
-        bins : list
-            list of bin boundaries, with length one element longer than data
-        mean : float
-
-    Returns:
-        std : float
-            Standard deviation
-    """
-    numerator = 0
-    totalCounts = 0
-    for ix, key in enumerate(data):
-        counts = data[ix]
-        lower = bounds[ix]
-        upper = bounds[ix+1]
-        difference = upper-lower
-        midpoint = lower + difference/2
-        totalCounts = totalCounts + counts
-        numerator = counts * ((midpoint - mean) ** 2) + numerator
-
-    denominator = totalCounts
-    std = np.sqrt(numerator / denominator)
-    return std, lower, upper
-
-
-def normalise(data, bins):
-    for key in bins:
-        value = bins[key]
-        if isinstance(value,  tuple):
-            if len(value) == 2:
-                lower = value[0]
-                upper = value[1]
-                difference = upper-lower
-                data[key] = data[key]/difference
-            else:
-                error = "The length of value of the key %s is %s,  must be 2" % (key, len(value))
-                print(error)
-        else:
-            error = "The type of value of the key %s is %s,  must be tuple e.g. (1, 2)" % (key, type(value))
-            print(error)
-    return data
 
 
 def geometricStd(data, bounds, gmd):
@@ -128,23 +131,31 @@ def geometricStd(data, bounds, gmd):
 
 
 def plot(data, path, name):
-    filename = name + "-plot.pdf"
-    path = os.path.join(path, filename)
-    print("Plotting: %s" % (path))
     plt.clf()
     fig = plt.figure()
     ax = fig.add_subplot(111)
     data.plot(ax=ax)
     plt.ylabel('Particle count')
     plt.xlabel('Time')
+    # custom_tick_labels = data.index.map(lambda t: t.strftime('%H'))
+    # ax.set_xticklabels(custom_tick_labels)
+
+    lines, labels = ax.get_legend_handles_labels()
+    ax.legend(lines, labels, loc='upper center', mode='expand')
+    art = []
+    lgd = ax.legend(lines, labels, loc=9, bbox_to_anchor=(0.5, -0.2), ncol=2)
+    art.append(lgd)
+    kwargs = {"additional_artists": art,
+              "bbox_inches": "tight"}
+
     plt.tight_layout()
-    fig.savefig(path)
+    path = saveplot(path, name, "plot", fig, **kwargs)
     plt.clf()
     # plt.close()
-    return path.replace("\\", "/")
+    return path.replace("\\\\", "/")
 
 
-def histogram(data, path, name):
+def histogram(data, plotspath, statspath, name):
     """Plot a histogram of a binned data with defined bin boundaries.
     Args:
         df : Pandas.DataFrame
@@ -213,10 +224,10 @@ def histogram(data, path, name):
     # logmidpoints = np.log10(midpoints)
 
     # Normal distribution
-    mean, std, lower, upper = stats(midpoints, counts)
+    mean, std, lower, upper = statistics(midpoints, counts)
 
     # Log normal distribution
-    gm, gstd, glower, gupper = np.exp(stats(np.log(midpoints), counts))
+    gm, gstd, glower, gupper = np.exp(statistics(np.log(midpoints), counts))
 
     # Sometimes a median is not found and so need to be excluded from display
     if 'median' in locals():
@@ -234,13 +245,13 @@ def histogram(data, path, name):
         statsdata = [mean, std, lower, upper,
                      gm, std, glower, gupper]
 
-    statistics = pd.DataFrame(statsdata, index=index)
-    statslatex = statistics.to_latex(header=False)
+    statsdf = pd.DataFrame(statsdata, index=index)
     columns = ['Counts', 'Cum Counts', 'Density']
 
-    datalatex = df1[columns].to_latex()
-    dataDict['histstats'] = statslatex
-    dataDict['histdata'] = datalatex
+    dataDict['histstats'] = save_latex(statsdf, statspath,
+                                       name, "histstats", header=False)
+    dataDict['histdata'] = save_latex(df1[columns], statspath,
+                                      name, "histdata")
 
     # Plots
     x1 = df1["lower"].tolist()  # left edge
@@ -250,13 +261,21 @@ def histogram(data, path, name):
 
     plt.clf()
     fig = plt.figure()
-    plt.bar(x1, y, width=w)
-    plt.ylabel(r'Frequency per $\mu$$')
+    ax = fig.add_subplot(111)
+    ax.bar(x1, y, width=w)
+
+    # find minimum and maximum of xticks, so we know
+    # where we should compute theoretical distribution
+    xt = plt.xticks()[0]
+    xmin, xmax = min(xt), max(xt)
+    lnspc = np.linspace(xmin, xmax, len(y))
+    pdf_g = stats.norm.pdf(lnspc, mean, std)
+    ax.plot(lnspc, pdf_g, label="Norm")
+
+    plt.ylabel(r'Frequency per $\mu$')
     plt.xlabel(r'\textbf{Diameter} / $\mu$')
-    filename1 = name + "-hist.pdf"
-    path1 = os.path.join(path, filename1)
-    dataDict['histplot']["hist"] = path1.replace("\\", "/")
-    fig.savefig(path1)
+    path1 = saveplot(plotspath, name, "hist", fig)
+    dataDict['histplot']["hist"] = path1
 
     # Cumulative distribution
     y = df1['Cum Counts'].tolist()
@@ -266,10 +285,8 @@ def histogram(data, path, name):
     plt.plot(x1, y)
     plt.ylabel(r'Cumulative frequency')
     plt.xlabel(r'\textbf{Diameter} / $\mu$')
-    filename1 = name + "-cumFreq.pdf"
-    path1 = os.path.join(path, filename1)
-    dataDict['histplot']["cumFreq"] = path1.replace("\\", "/")
-    fig.savefig(path1)
+    path1 = saveplot(plotspath, name, "cumFreq", fig)
+    dataDict['histplot']["cumFreq"] = path1
 
     # x1 = df1["loglower"].tolist() # left edge
     # x2 = df1["logupper"].tolist() # right edge
@@ -280,15 +297,21 @@ def histogram(data, path, name):
     ax = fig.add_subplot(111)
     ax.bar(x1, y, width=w)
     ax.set_xscale('log')
-    # ax.set_yscale('log')
+
+    # find minimum and maximum of xticks, so we know
+    # where we should compute theoretical distribution
+    xt = plt.xticks()[0]
+    xmin, xmax = min(xt), max(xt)
+    lnspc = np.linspace(xmin, xmax, len(y))
+    pdf_g = stats.norm.pdf(lnspc, gm, gstd)
+    ax.plot(lnspc, pdf_g, label="Norm")
+
     plt.ylabel(r'Frequency per $\log \mu$')
     plt.xlabel(r'$\log \mathbf{Diameter}$ / $\mu$')
-
-    filename2 = name + "-histlog.pdf"
-    path2 = os.path.join(path, filename2)
-    dataDict['histplot']["histlog"] = path2.replace("\\", "/")
-    fig.savefig(path2)
+    path1 = saveplot(plotspath, name, "histlog", fig)
+    dataDict['histplot']["histlog"] = path1
     plt.clf()
+
     return dataDict
 
 
@@ -308,9 +331,7 @@ def plot_corr(df, plotspath, statspath, name, size=14):
     locs, labels = plt.xticks(range(len(corr.columns)), corr.columns)
     plt.setp(labels, rotation=90)
     plt.yticks(range(len(corr.columns)), corr.columns)
-    filename = name + "-corr.pdf"
-    path = os.path.join(plotspath, filename)
-    fig.savefig(path)
+    path1 = saveplot(plotspath, name, "corr", fig)
     print("Saved correlations graph")
     plt.clf()
 
@@ -327,9 +348,7 @@ def plot_corr(df, plotspath, statspath, name, size=14):
     locs, labels = plt.xticks(range(len(cov.columns)), cov.columns)
     plt.setp(labels, rotation=90)
     plt.yticks(range(len(cov.columns)), cov.columns)
-    filename = name + "-cov.pdf"
-    path = os.path.join(plotspath, filename)
-    fig.savefig(path)
+    path1 = saveplot(plotspath, name, "cov", fig)
     print("Saved covariations graph")
 
 
@@ -352,15 +371,10 @@ def make_scatter_plot(df, path, name):
     for ax in axs[-1, :]:  # the lower boundary
         ax.grid('off', axis='both')
         # ax.set_xticks([0, .5])
-    filename = name + "-scatter-matrix.pdf"
-    path = os.path.join(path, filename)
-    fig.savefig(path)
+    path1 = saveplot(path, name, "scatter-matrix", fig)
 
 
 def scatter_matrix(df, path, name):
-    filename = name + "-scatter-matrix.pdf"
-    path = os.path.join(path, filename)
-
     fig, axarr = plt.subplots(4, 4, sharex=True)
 
     x = df.columns[0]
@@ -377,7 +391,7 @@ def scatter_matrix(df, path, name):
     # # the lower boundary
     # axarr[-1, :].grid('off', axis='both')
     # axarr[-1, :].set_xticks([0,  .5])
-    fig.savefig(path)
+    path1 = saveplot(path, name, "scatter-matrix", fig)
 
 
 def scatter_plot(df, path, name):
@@ -393,3 +407,39 @@ def scatter_plot(df, path, name):
 def compare(data, path):
     plot_corr(data, path)
     # scatter_plot(data, path)
+
+
+def calibrate(df):
+    """Calibrate a dataset by other dataset.
+    Each dataset is a time series histogram with defined bin boundaries.
+    Both datasets must share same bin boundaries.
+    The value of a bin of a dataset is divided by a value of same bin of
+    different dataset to find a multiplicative calibration factor between two
+    dataset for that bin.
+
+    Args:
+        df: Pandas.DataFrame
+            Input dataframe consisting of two datasets, where each column is a
+            bin. For two bins dataset, the dataframe will have four columns, of
+            which two belonged to one dataset and other two belonged to another
+            dataset.
+            The leftmost dataset will be divided by the rightmost dataset.
+    Returns:
+        df: Pandas.DataFrame
+            Output DataFrame is a superset of input dataframe with additional
+            columns which are calibration factors for each bin over time.
+    """
+    columns = df.columns
+    for i, a in enumerate(columns):
+        for j, b in enumerate(columns[i:]):
+            sensor1, bin1 = a.split('-')
+            sensor2, bin2 = b.split('-')
+            if bin1 == bin2 and sensor1 != sensor2:
+                # Ensure the bins are same but sensors are different
+                c = "%s/%s-%s" % (sensor1, sensor2, bin1)
+                df[c] = df[a] / df[b]
+
+    # Sometimes there are zero counts so will yield NaN or Inf
+    df = df.fillna(0)
+    df = df.replace(np.inf, 0)
+    return df
