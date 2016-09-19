c0d33518f4ea317ffed4e2bdca17f8d016b9dc39
DancingQuanta <andytheseeker@gmail.com>
2016-09-19 16:57:15 +0100

    Process_alpha.py Changed sampling period to 10. Compute concentration using period and flow rate. resample to minute More fuller description

--- src/sensors/process_alpha.py
+++ src/sensors/process_alpha.py
@@ -5,25 +5,39 @@
 Process alphasense dataset
 Alphasense data is produced when Raspberry pi asks for it.
 
-The sensor output data with a Sampling Period value which is the length of time
-between sampling.
-The sampleing period is normally less than the sampling period of the raspberry
+The sensor output data consisting of a histogram of 16 bins, sampling period
+and sample flow rate measured each sample cycle.
+The histogram produced by the sensor is a total counts of particles in each bin
+for a whole sample period.
+The sampling period is the length of time of counting particles.
+The sampling period is normally less than the sampling period of the raspberry
 pi because of the delay in communication.
 The delay is about 0.1 seconds.
-When the raspberry pi starts to sample data from the sensor, the first
-datapoint have a sampling period less than the expected sampling period and so
-can be rejected.
+The sample flow rate is the rate of volumn of air through the sensor.
+The sampling period must be between 1.4 and 10 seconds.
 
-The histogram produced by the sensor is a total counts of particles in each bin
-for a whole sample, between sampling.
+When the raspberry pi starts to sample data from the sensor, the first
+datapoint have a sampling period very different from the expected sampling
+period and so should be rejected.
+
 To obtain the particle concentration, divide the total by the product of the
 flow rate and sampling period.
 
-The sample flow rate of Alphasense OPC-N2 is 220 ml/min.
+The sample flow rate of Alphasense OPC-N2 is normally 220 ml/min.
 
-This scripts removes data with wrong sampling periods and convert the histogram
-to particle concentration in 1/cm3.
+The function of this script are:
+
+    Removes data with wrong sampling periods
+
+    Converts the histogram to particle concentration in 1/cm3.
+
+    Resample the data to 1 minute frequency.
+
+    Checks for any gaps in time and produce separate datafiles of data that do
+    not have time gaps.
+
 """
+
 import os
 import sys
 from operator import itemgetter
@@ -35,7 +49,11 @@ from pint import UnitRegistry
 # Initialise pint's UnitRegistry
 ureg = UnitRegistry()
 
+
 def argparse(argv):
+    """Parse commandline arguments.
+    Expects input file path
+    """
     try:
         inputfile = str(argv[1])
     except:
@@ -59,7 +77,7 @@ def loadData(path):
             'Bin 12', 'Bin 13', 'Bin 14', 'Bin 15',
             'Bin1 MToF', 'Bin3 MToF', 'Bin5 MToF', 'Bin7 MToF',
             'Sampling Period', 'Temperature',
-            'Pressure', 'PM1', 'PM2.5', 'PM10']
+            'Pressure', 'PM1', 'PM2.5', 'PM10', 'SFR']
     # Load data
     df = pd.read_csv(path,
                      parse_dates=[0],
@@ -72,7 +90,12 @@ def loadData(path):
 
 
 def concentration(df):
+    """
+    Convert total counts to concentration using sampling period and sample
+    flow rate measured during samping
+    """
     print("Converting histogram to concentration")
+
     cols = ['Bin 0', 'Bin 1', 'Bin 2', 'Bin 3',
             'Bin 4', 'Bin 5', 'Bin 6', 'Bin 7',
             'Bin 8', 'Bin 9', 'Bin 10', 'Bin 11',
@@ -86,7 +109,10 @@ def concentration(df):
     # not been multipled with magnitude of sampling period yet,
 
     # Divide the histogram
-    df['Total Volume'] = df['Sampling Period'] * vol
+    samplingperiod = ureg('s') * df['Sampling Period']
+    flowrate = ureg('ml/s') * df['SFR']
+    vol = flowrate * samplingperiod
+    df['Total Volume'] = (vol).to('cm ** 3').magnitude
     df[cols] = df[cols].div(df['Total Volume'], axis=0)
 
     print("Successfully converted")
@@ -106,36 +132,43 @@ def findGroups(index):
 
 
 def main(inputfile):
-    """
-    Group data separated by rows with period between 59 and 65 seconds.
-    """
     print("Processing data!")
     # load data
     data = loadData(inputfile)
 
+    # Find index of rows with period between 9 and 11 seconds.
+    index = [i for i, x in enumerate(data['Sampling Period'].values)
+             if not 9 < x < 11]
+    # Drop rows with incorrect periods
+    data = data.drop(data.index[index])
+
     # Calculate concentration
     data = concentration(data)
+
+    # Resample to minute frequency
+    data = data.resample('T', label='left', closed='left').mean()
+
+    # Find segments between Nan
+    date_range = pd.date_range(min(data.index), max(data.index), freq='T')
+    data = data.reindex(date_range)
+    ts = data[data.columns[0]]
+    index = np.where(ts.notnull())[0]
+    ranges = findGroups(index)
 
     # Generate path to split data
     name = os.path.basename(inputfile)
     name = os.path.splitext(name)[0]
-
     path = os.path.abspath(inputfile)
     path = os.path.dirname(path)
     path = os.path.join(path, "split-" + name)
     if not os.path.isdir(path):
         os.mkdir(path)
+
+    # Open a file to write processed data
     path1 = os.path.join(path, name)
-    fid = open(path1,"a")
+    fid = open(path1, "a")
 
-    # Find index of rows with period between 59 and 65 seconds.
-    period = data['Sampling Period']
-    # index = data[59 < data['Sampling Period'] < 65].index.tolist()
-    # index = np.where(59 < period < 65)[0]
-
-    index = [i for i, x in enumerate(period) if 59 < x < 65]
-
-    ranges = findGroups(index)
+    # Analyse data for time gaps (NaN)
     if len(ranges) != 0:
         start,  end = zip(* ranges)
         startdt = data.index[list(start)]
@@ -152,12 +185,11 @@ def main(inputfile):
             path2 = os.path.join(path, filename)
             segment.to_csv(path2, header=None)
             segment.to_csv(fid, header=None)
-        return df
+        print(df)
     else:
         print("All Nan!")
         return None
     fid.close()
-
 
 if __name__ == "__main__":
     inputfile = argparse(sys.argv)
