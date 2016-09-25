ccc2d7638ea68d6705932fabaead7d177aaa76f0
DancingQuanta <andytheseeker@gmail.com>
2016-09-25 17:03:44 +0100

    Nearly made the hitt matrix plot i wanted

--- src/visualisation/hist_matrix.py
+++ src/visualisation/hist_matrix.py
@@ -45,22 +45,6 @@ def histogram(df, bounds, ax):
     ax.set_xscale('log')
 
 
-def set_ax(ax, x_title, y_title):
-    ax.xaxis.set_visible(False)
-    if ax.is_first_col():
-        ax.annotate(y_title, xy=(0, 0.5),
-                    xycoords=('axes fraction', 'axes fraction'),
-                    xytext=(10, 0),
-                    textcoords='offset points',
-                    size=14, ha='center', va='bottom')
-    # if ax.is_last_col():
-
-    if ax.is_first_row():
-        ax.set_title(x_title)
-    if ax.is_last_row():
-        ax.xaxis.set_ticks_position('bottom')
-
-
 if __name__ == '__main__':
 
     # Get filenames to work with
@@ -68,20 +52,10 @@ if __name__ == '__main__':
     parser.add_argument("settings", help="Date file")
     parser.add_argument("-o", "--output",
                         help="Directs the output to a name of your choice")
-    parser.add_argument("-f", "--figsize",
-                        help="Figure size")
 
     options = parser.parse_args()
     settings_file = options.settings
     output_file = options.output
-    fig_size = options.figsize
-
-    # Set figure size
-    if fig_size is not None:
-        params = {"figure.figsize": figsize(float(fig_size))}
-    else:
-        params = {"figure.figsize": figsize(0.49)}
-    matplotlib.rcParams.update(params)
 
     # Load settings yaml file
     with open(settings_file) as handle:
@@ -98,17 +72,42 @@ if __name__ == '__main__':
     exp_order = exps['order']
     conditions = exps['conditions']
 
-    fig, axarr = plt.subplots(nrows=len(exp_order), ncols=len(sensor_order))
+    # Matrix dimension
+    nrows=len(exp_order)
+    ncols=len(sensor_order)
+
+    # Fig size
+    fig_width = 2.2
+    fig_height = 1.6
+    fig_size = [(fig_width * ncols), (fig_height * nrows)]
+    params = {"figure.figsize": fig_size}
+    matplotlib.rcParams.update(params)
+
+    fig, axarr = plt.subplots(nrows=nrows, ncols=ncols)
     axes = fig.axes
-    fig.subplots_adjust(hspace=0.05, wspace=0.05)
+    # fig.subplots_adjust(hspace=0.05, wspace=0.05)
+    x_label = r'Diameter / \si{\um}'
+    y_label = r'Frequency per $\log D$'
+
     k = 0
-    for exp in exp_order:
-        condition = conditions[exp]
-        for sensor in sensor_order:
+    plt.annotate(x_label, xy=(0.5, 0),
+                xycoords=('figure fraction', 'figure fraction'),
+                xytext=(0, 6),
+                textcoords='offset points',
+                ha='center', va='center')
+    plt.annotate(y_label, xy=(0, 0.5),
+                xycoords=('figure fraction', 'figure fraction'),
+                xytext=(10, 0),
+                textcoords='offset points',
+                ha='center', va='center',
+                rotation='vertical')
+    for i, sensor in enumerate(sensor_order):
+        for j, exp in enumerate(exp_order):
+            condition = conditions[exp]
             # Get name
             sensor_name = 'test'
-            sensor_name = sensors[sensor]['name']
-            condition_name = condition
+            x_title = sensors[sensor]['name']
+            y_title = r"\SI{%s}{\um}" % (exp)
 
             # load data
             data_path = condition['sensor'][sensor]['data']
@@ -118,18 +117,27 @@ if __name__ == '__main__':
             bounds = sensors[sensor]['bins']
 
             # Pick an axes
-            ax = axes[k]
+            ax = axarr[j,i]
 
             # Histogram logdensity
             histogram(df, bounds, ax)
 
-            # Plot
-            # set_ax(ax, sensor_name, condition_name)
+            # Setting labels
+            ax.xaxis.set_visible(False)
+            if i == 0:
+                ax.annotate(y_title, xy=(0, 0.5),
+                            xycoords=('axes fraction', 'axes fraction'),
+                            xytext=(-40, 0),
+                            textcoords='offset points',
+                            size=12, ha='center', va='bottom')
+
+            if j == 0:
+                ax.set_title(x_title)
+
+            if j == nrows:
+                ax.xaxis.set_ticks_position('bottom')
             k += 1
 
-    x_label = r'$\mathbf{Diameter}$ / $\mu$'
-    y_label = r'Frequency per $\log D$'
-    fig.text(0.5, 0.04, x_label, ha='center', va='center')
-    fig.text(0.06, 0.5, y_label, ha='center', va='center', rotation='vertical')
-    saveplot(output_file, fig)
+    kwargs = {"bbox_inches": "tight"}
+    saveplot(output_file, fig, **kwargs)
     plt.close()
