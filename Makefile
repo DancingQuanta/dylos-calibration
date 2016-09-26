0fd2e97591a52b64bd334c862f81f57876c333b2
DancingQuanta <andytheseeker@gmail.com>
2016-09-26 14:47:54 +0100

    Added plot_matrix

--- Makefile
+++ Makefile
@@ -18,15 +18,22 @@ YAML := $(shell find $(SETTINGS)/ -name '*.yaml')
 INTERIM_SETTINGS := $(patsubst $(SETTINGS)/%.yaml,$(INTERIM)/%.json,$(YAML))
 INTERIM_DATA := $(shell find $(INTERIM)/ -name '*.csv')
 CALIBRATE_FLAGS := $(patsubst $(INTERIM)/%.json,.calibrated-%,$(INTERIM_SETTINGS))
+
 PLOT_DATA := $(patsubst $(INTERIM)/%.csv,$(IMGS)/%-plot.png,$(INTERIM_DATA))
+PLOT_MAT := $(patsubst $(INTERIM)/%.json,$(IMGS)/%-plot-mat.png,$(INTERIM_SETTINGS))
+PLOT_MAT_PDF := $(patsubst $(INTERIM)/%.json,$(PROCESSED)/%-plot-mat.pdf,$(INTERIM_SETTINGS))
+PLOT_MAT_TEMPLATE := templates/plot_mat.tpl
+
 HIST_PLOT := $(patsubst $(INTERIM)/%.csv,$(IMGS)/%-hist.png,$(INTERIM_DATA))
 HIST_STATS := $(patsubst $(INTERIM)/%.csv,$(PROCESSED)/%-stats.tex,$(INTERIM_DATA))
 HIST_MAT := $(patsubst $(INTERIM)/%.json,$(IMGS)/%-hist-mat.png,$(INTERIM_SETTINGS))
 HIST_MAT_PDF := $(patsubst $(INTERIM)/%.json,$(PROCESSED)/%-hist-mat.pdf,$(INTERIM_SETTINGS))
 HIST_MAT_TEMPLATE := templates/hist_mat.tpl
+
 PROCESS_SCRIPT := src/data/process.py
 CALIBRATION_SCRIPT := src/data/calibration.py
 PLOT_SCRIPT := src/visualisation/plot.py
+PLOT_MAT_SCRIPT := src/visualisation/plot_matrix.py
 HIST_SCRIPT := src/visualisation/hist.py
 HIST_MAT_SCRIPT := src/visualisation/hist_matrix.py
 GEN_SCRIPT := src/docs/gen.py
@@ -51,6 +58,8 @@ data: $(INTERIM_SETTINGS)
 calibrate: $(CALIBRATE_FLAGS)
 
 plot: $(PLOT_DATA)
+
+plotmat: $(PLOT_MAT_PDF)
 
 hist: $(HIST_PLOT)
 
@@ -78,9 +87,17 @@ $(INTERIM)/%.json: $(SETTINGS)/%.yaml $(SENSORS) $(DATA) $(PROCESS_SCRIPT)
 	python $(CALIBRATION_SCRIPT) $<
 	touch .calibrated-$*
 
+# Time series plots
 $(IMGS)/%-plot.png: $(INTERIM)/%.csv $(PLOT_SCRIPT)
 	python $(PLOT_SCRIPT) $< -o $@ -f $(FIG_SIZE)
 
+$(IMGS)/%-plot-mat.png: $(INTERIM)/%.json $(DATA) $(PLOT_MAT_SCRIPT)
+	python $(PLOT_MAT_SCRIPT) $< -o $@
+
+$(PROCESSED)/%-plot-mat.tex: $(INTERIM)/%.json $(PLOT_MAT_TEMPLATE) $(PLOT_MAT)
+	python $(GEN_SCRIPT) $(PLOT_MAT_TEMPLATE) $< $@ 
+
+# Histogram plots
 $(IMGS)/%-hist.png $(PROCESSED)/%-hist.csv: $(INTERIM)/%.csv $(HIST_SCRIPT)
 	python $(HIST_SCRIPT) $< -p $(IMGS)/$*-hist.png -s $(PROCESSED)/$*-hist.csv -f $(FIG_SIZE)
 
---
+++ src/visualisation/plot_matrix.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import pandas as pd
+import argparse
+import json
+from utils import *
+import logging
+
+logging.basicConfig(filename='log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+
+def plot(df, ax):
+    # Change the index of df to minutes
+    df = index_mins(df)
+
+    # Plot timeseries
+    df.plot(ax=ax)
+
+
+if __name__ == '__main__':
+
+    # Get filenames to work with
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("settings", help="Date file")
+    parser.add_argument("-o", "--output",
+                        help="Directs the output to a name of your choice")
+
+    options = parser.parse_args()
+    settings_file = options.settings
+    output_file = options.output
+
+    # Load settings yaml file
+    with open(settings_file) as handle:
+        settings = json.load(handle)
+
+    # Load sensor info
+    calibrater = settings['calibration']['calibrater']
+    calibratee = settings['calibration']['calibratee']
+    rebinned = settings['calibration']['rebinned']
+    sensors = settings['sensors']
+
+    # Experimental conditions
+    exps = settings['exp']
+
+    # Conditions
+    exp_order = exps['order']
+    conditions = exps['conditions']
+
+    # Matrix dimension
+    nrows=len(exp_order)
+    ncols=2
+
+    # Fig size
+    fig_width = 2
+    fig_height = 1.6
+    fig_size = [(fig_width * ncols), (fig_height * nrows)]
+
+    # Create matrix with first columns for particles
+    fig = plt.figure(figsize=fig_size)
+    width_ratios = [1] + ncols * [2]
+    gs = gridspec.GridSpec(nrows, (ncols + 1),
+                           width_ratios=width_ratios,
+                           wspace=0.5, hspace=0.10)
+    x_label = r'Time / min'
+    y_label = r'Particle counts'
+
+    # First column
+    ax = plt.subplot(gs[0, 0])
+    ax.set_title("Particle size")
+    ax.annotate(y_label, xy=(1, 0.5),
+                xycoords=('axes fraction', 'figure fraction'),
+                xytext=(10, 0),
+                textcoords='offset points',
+                ha='center', va='center',
+                rotation='vertical')
+
+    for i, exp in enumerate(exp_order):
+        condition = conditions[exp]
+        # Get name
+        y_title = r"\SI{%s}{\um}" % (exp)
+
+        # First column
+        ax = plt.subplot(gs[i, 0])
+        ax.annotate(y_title, xy=(0.5, 0.5),
+                    xycoords=('axes fraction', 'axes fraction'),
+                    xytext=(0, 0),
+                    textcoords='offset points',
+                    size=11, ha='center', va='bottom')
+        ax.axis('off')
+
+        # Second column
+        # load calibrater data and plot it!
+        data_path = condition['sensor'][calibrater]['data']
+        df = load_data(data_path)
+
+        # Pick an axes
+        ax = plt.subplot(gs[i, 1])
+
+        # Plot timeseries
+        plot(df, ax)
+
+        # Third column
+        # load calibratee and rebinned data and plot it!
+        data_path = condition['sensor'][calibratee]['data']
+        df = load_data(data_path)
+        print(df)
+
+        data_path = condition['sensor'][rebinned]['data']
+        df1 = load_data(data_path)
+        df = concat(df,df1)
+
+        # Pick an axes
+        ax = plt.subplot(gs[i, 2])
+
+        # Plot timeseries
+        plot(df)
+
+
+    kwargs = {"bbox_inches": "tight"}
+    settings['plots'] = {}
+    settings['plots']['plot-mat'] = saveplot(output_file, fig, **kwargs)
+    plt.close()
+    # Dump the plot path to json
+    dump = json.dumps(settings, default=date_handler,
+                     sort_keys=True, indent=4).replace("\\\\", "/")
+    with open(settings_file, 'w') as handle:
+            handle.write(dump)
+
+
+#     lines, labels = ax.get_legend_handles_labels()
+    # # ax.legend(lines, labels, loc='upper center', mode='expand')
+    # art = []
+    # lgd = ax.legend(lines, labels, loc=9, bbox_to_anchor=(0.5, -0.2), ncol=2)
+    # art.append(lgd)
+    # kwargs = {"additional_artists": art,
+              # "bbox_inches": "tight"}
+
+    # path = saveplot(path, fig, **kwargs)
+
